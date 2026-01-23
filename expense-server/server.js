867b0b214a4d9c13a58ff52066a03ae948d13bbe
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-23 13:18:57 +0530

    commit

---
+++ expense-server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); //Middleware
+
+let users = [];
+
+app.post('/register', (request, response) => {
+    const { name, email, password } = request.body;
+
+
+    if (!name || !email || !password) {
+        return response.status(400).json({
+            message: 'Name, Email, Password are required '
+        });
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name: name,
+        email: email,
+        password: password
+    };
+    users.push(newUser);
+
+    return response.status(200).json({
+        message: 'userregisterd',
+        users: {
+            id: newUser.id
+        }
+    });
+});
+app.listen(5001, () => {
+    console.log(' Server is running in port 5001');
+});
